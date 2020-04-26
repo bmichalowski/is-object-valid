e2579e22dbf900b358416959c6ed76f5e4ba713f
bmichalowski <bogusz.michalowski@gmail.com>
2020-04-26 20:23:55 +0200

    Change  tests extension to 'test'

--- src/schema/schema.spec.js
+++
@@ -1,8 +0,0 @@
-import { schema } from './schema';
-
-describe('schema', ()=>{
-    it('should have isValid function', ()=>{
-        const testObj = schema({});
-        expect(typeof testObj.isValid).toBe('function');
-    });    
-});
